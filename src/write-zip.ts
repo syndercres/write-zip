f269b4a34e78209c80b28bf2b20fffaccd8b2a99
syndercombecreswell <syndercombecreswell@gmail.com>
2022-10-31 10:01:44 +0000

    pseudocode for zip

---
+++ src/write-zip.ts
@@ -0,0 +1,38 @@
+import { generateKeyPair } from "crypto"
+
+/*
+
+function shortest:
+
+parameters: array of numbers or strings x2
+
+return type: number
+
+create variable; shortest = array1.length
+
+if array2.length is shorter, reassign shortest to array 2
+
+return shortest;
+
+
+------------------------------------------------------------------------
+
+function zip:
+
+parameters array of numbers, array of numbers
+
+return type: array of tuples
+
+-----------------------------------------------------------------------------
+
+
+arrayLength = shortest(Arr1,Arr2) // number
+
+returnArr = [] //array of tuples
+
+
+for i=0 to i=arrayLength 
+    let tuple = [Arr1[i],Arr2[i]]
+    push tuple to returnArr
+
+    /*
